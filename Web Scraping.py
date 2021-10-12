3b8f2bb08ca5e468109d0859df4c5f7082c059a0
praveenmundkar <pravinbiloli123@gmail.com>
2021-10-12 18:16:41 +0530

    Improved the readability of code

--- Web Scraping.py
+++ Web Scraping.py
@@ -1,3 +1,8 @@
+""" Installing Required modules
+    pip install selenium
+    pip install webdriver_manager """
+
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager                    # ChromeDriveManager will automatically deal with the driver required
 from selenium.webdriver.common.by import By
@@ -23,9 +28,9 @@ nn = 0
 for cat in category:
     print(cat)                                                               # Printing the category
     list_of_sub_jobs = sub_category[nn].split("\n")
-    print(list_of_sub_jobs)                                                  # Jobs under the category printed before
+    print(list_of_sub_jobs)                                                  # Jobs under the category printed by line 29
     nn += 1
-    print("-"*50)
+    print("-"*100)
     driver.get("https://in.linkedin.com/jobs")                                 # Getting the linkedin server to scrap required data
     driver.find_element(By.XPATH, "//input[contains(@name,'location')]").clear()        # clear if there is any auto-input
     driver.find_element(By.XPATH, "//input[contains(@name,'location')]").send_keys("India")                # Sending location key as india
@@ -79,13 +84,15 @@ for cat in category:
 
             desc = desc.text
 
-            fe = driver.find_elements(By.XPATH, "//dd[contains(@class,'basic-info-item__description')]")                                         # Scraping ther info. i.e no. of employees and headquater location
+            fe = driver.find_elements(By.XPATH, "//dd[contains(@class,'basic-info-item__description')]")                                         # Scraping their info. i.e no. of employees and headquater location
             for fe in fe:
                 # print(fe.text)
                 f_e.append(fe)
             employees_locations.append(f_e[2])
             employees_locations.append(f_e[3])
             driver.implicitly_wait(5)
+
+            """ Printing the Information that we required from our desired output """
 
             print(job_title_list[mm])
             print(company_list[mm])
@@ -95,17 +102,20 @@ for cat in category:
             print("Headquaters at :: ", employees_locations[1].text)
             mm = mm + 1
             links = ""
-
-            driver.close()                                                                      # Closing the child window which is now no. longer required to avoid some errors
-            driver.switch_to.window(parentwindow)
+            print("*"*100)
+            print("*"*100)
+            driver.close()                                                                      # Closing the child window which is now no. longer required to avoid conflicting errors due to the window
+            driver.switch_to.window(parentwindow)                                               # Re-switching to the parent window
 
         except:
-            print("occured some error")
+            print("occurred some error")                                                        # The errors may be like while we click on company page by line 75 it may get unwanted popup windows or the page may not available, etc
+            print("*" * 100)
+            print("*" * 100)
             pass
 
         finally:
-            break         # break by getting one job profile(i.e sub-category) for each category NOTE  ::: REMOVE THE finally BLOCK to get every JOB info of each CATEGORY
+            break                                                    # break by getting one job profile(i.e sub-category) for each category 🔲 NOTE  ::: REMOVE THE finally BLOCK to get every JOB info of each CATEGORY
 
 
-driver.quit()
+driver.quit()                                                        # Exit the program by closing all windows
 
