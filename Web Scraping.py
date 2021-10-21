58f80ed8c9e8417b10b55ed410007662a3ba2ef5
praveenmundkar <pravinbiloli123@gmail.com>
2021-10-21 12:21:42 +0530

    connected eith database and improved the code

--- Web Scraping.py
+++ Web Scraping.py
@@ -2,49 +2,86 @@
     pip install selenium
     pip install webdriver_manager """
 
-
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager                    # ChromeDriveManager will automatically deal with the driver required
+from webdriver_manager.chrome import \
+    ChromeDriverManager  # ChromeDriveManager will automatically deal with the driver required
 from selenium.webdriver.common.by import By
+import mysql.connector
 
-driver = webdriver.Chrome(ChromeDriverManager().install())                  # installing the driver required
+conn = mysql.connector.connect(user="root", host="localhost", password="Rocky@222", database="mydata")
+cursor = conn.cursor()
 
-driver.maximize_window()                                                    # Maximizing the Window
+cursor.execute("truncate table categories")
+cursor.execute("truncate table subcategories")
+cursor.execute("truncate table states")
+cursor.execute("truncate table company_details")
+cursor.execute("truncate table jobs")
+conn.commit()
 
-driver.get("https://www.careerguide.com/career-options")                    # Getting the server
+driver = webdriver.Chrome(ChromeDriverManager().install())  # installing the driver required
+
+driver.maximize_window()  # Maximizing the Window
+
+category = []  # Initializing empty list to store categories
+sub_category = []  # Initializing empty list to store categories
 
 
-r = driver.find_elements(By.XPATH, "//h2[contains(@class,'c-font-bold')]")  # Scraping the categories of Jobs
-sub_r = driver.find_elements(By.XPATH, "//ul[contains(@class,'c-content-list-1')]")  # Scraping the sub-categories of Jobs
+def login(email, password):
+    url = "https://www.linkedin.com/"
+    driver.get(url)
+    driver.implicitly_wait(5)
+    # driver.maximize_window()
+    driver.find_element_by_id('session_key').send_keys(email)
+    driver.find_element_by_id('session_password').send_keys(password)
+    driver.find_element_by_class_name('sign-in-form__submit-button').click()
+    driver.implicitly_wait(5)
 
-category = []                                                               # Initializing empty list to store categories
-sub_category = []                                                           # Initializing empty list to store categories
-for ele in r:
-    category.append(ele.text)
-for ele in sub_r:
-    sub_category.append(ele.text)
-ss = 0
+
+def extract_cat_sub_cat():
+    driver.get("https://www.careerguide.com/career-options")  # Getting the server
+
+    r = driver.find_elements(By.XPATH, "//h2[contains(@class,'c-font-bold')]")  # Scraping the categories of Jobs
+    sub_r = driver.find_elements(By.XPATH,
+                                 "//ul[contains(@class,'c-content-list-1')]")  # Scraping the sub-categories of Jobs
+
+    for ele in r:
+        category.append(ele.text)
+    for ele in sub_r:
+        sub_category.append(ele.text)
+
+    return category, sub_category
+
+
+extract_cat_sub_cat()
+login("movietrailerscom7@gmail.com", "Qwerty@123")
+
 nn = 0
-for cat in category:
-    print(cat)                                                               # Printing the category
+bb = 0
+for cat in category[0:10]:
+    print(cat)  # Printing the category
     list_of_sub_jobs = sub_category[nn].split("\n")
-    print(list_of_sub_jobs)                                                  # Jobs under the category printed by line 29
-    nn += 1
-    print("-"*100)
-    driver.get("https://in.linkedin.com/jobs")                                 # Getting the linkedin server to scrap required data
-    driver.find_element(By.XPATH, "//input[contains(@name,'location')]").clear()        # clear if there is any auto-input
-    driver.find_element(By.XPATH, "//input[contains(@name,'location')]").send_keys("India")                # Sending location key as india
-    driver.find_element(By.XPATH, "//input[contains(@placeholder,'Search job titles or companies')]").send_keys(list_of_sub_jobs[0])            # Sending subcategory key as input
-    driver.find_element(By.XPATH, "//button[contains(@data-searchbar-type,'JOBS')]").click()                                                # Click search job button                                                  # scraping the results
+    print(list_of_sub_jobs)  # Jobs under the category printed by line 29
+    print("-" * 100)
+    driver.get("https://www.linkedin.com/jobs/")  # Getting the linkedin server to scrap required data
+    driver.implicitly_wait(5)
+    driver.find_element(By.XPATH,
+                        "//section[contains(@class,'msg-overlay-bubble-header__details flex-row align-items-center ml1')]").click()
+    driver.find_element(By.XPATH, "// input[contains( @ id, 'jobs-search-box-key')]").send_keys(list_of_sub_jobs[0])
+
+    driver.find_element(By.XPATH, "//input[contains(@id,'jobs-search-box-location')]").send_keys("India")
+    driver.find_element(By.XPATH, "//button[contains(@class,'jobs-search-box__submit-button')]").click()
+
     job_title_list = []
     company_list = []
     location_list = []
 
     """ Scraping the data from the results obtained after we clicked on search jobs button """
 
-    job_titles = driver.find_elements(By.XPATH, "//h3[contains(@class,'base-search-card__title')]")
-    companies = driver.find_elements(By.XPATH, "//h4[contains(@class,'base-search-card__subtitle')]")
-    locations = driver.find_elements(By.XPATH, "//span[contains(@class,'job-search-card__location')]")
+    job_titles = driver.find_elements(By.XPATH,
+                                      "//a[contains(@class,'disabled ember-view job-card-container__link job-card-list__title')]")
+    companies = driver.find_elements(By.XPATH,
+                                     "//a[contains(@class,'job-card-container__link job-card-container__company-name ember-view')]")
+    locations = driver.find_elements(By.XPATH, "//li[contains(@class, 'job-card-container__metadata-item')]")
 
     """ Storing the respective info. i.e job titles to the respective empty list created """
 
@@ -54,68 +91,61 @@ for cat in category:
         company_list.append(company.text)
     for location in locations:
         location_list.append(location.text)
+
     mm = 0
-
-    """ Scraping the links of the jobs to get more about them by iterating one by one i.e company description, no. of employees, headquater location of the respective company"""
-
-    links = driver.find_elements(By.XPATH, "//a[contains(@class,'base-card__full-link')]")
-
-    """ Iterating the links scraped to get the data required and then display them """
-
-    for link in links:
-        employees_locations = []
-        f_e = []
-        desc = ""
-
-        """ Using try block to ensure if we do not get the company page or any redirected sesions can be handled easily """
-
+    for profile in companies[0:1]:
         try:
-            link.click()                                                                                                                        # Click the result
-            driver.implicitly_wait(5)
-            driver.find_element(By.XPATH, "//a[contains(@class,'topcard__org-name-link')]").click()                                             # click company profile link
-
-            """ Handling the new created window i.e the child window """
-
-            childwindow = driver.window_handles[1]
-            parentwindow = driver.window_handles[0]
-            driver.switch_to.window(childwindow)                                                                                                 # Switching to the child window
-
-            desc = driver.find_element(By.XPATH, "//p[contains(@class,'about-us__description')]")                                                # scrape description
-
-            desc = desc.text
-
-            fe = driver.find_elements(By.XPATH, "//dd[contains(@class,'basic-info-item__description')]")                                         # Scraping their info. i.e no. of employees and headquater location
-            for fe in fe:
-                # print(fe.text)
-                f_e.append(fe)
-            employees_locations.append(f_e[2])
-            employees_locations.append(f_e[3])
-            driver.implicitly_wait(5)
-
-            """ Printing the Information that we required from our desired output """
-
-            print(job_title_list[mm])
-            print(company_list[mm])
-            print(location_list[mm])
-            print("Description about the company", desc)
-            print("No of employees", employees_locations[0].text)
-            print("Headquaters at :: ", employees_locations[1].text)
-            mm = mm + 1
-            links = ""
-            print("*"*100)
-            print("*"*100)
-            driver.close()                                                                      # Closing the child window which is now no. longer required to avoid conflicting errors due to the window
-            driver.switch_to.window(parentwindow)                                               # Re-switching to the parent window
-
+            profile.click()
         except:
-            print("occurred some error")                                                        # The errors may be like while we click on company page by line 75 it may get unwanted popup windows or the page may not available, etc
-            print("*" * 100)
-            print("*" * 100)
-            pass
+            profile.click()
+        driver.implicitly_wait(5)
+        about = driver.find_elements(By.XPATH,
+                                     "//a[contains(@class, 't-16 t-bold t-black--light org-page-navigation__item-anchor ember-view pv3 ph4')]")
+        for ab in about:
+            ab.click()
+            break
+        driver.implicitly_wait(5)
+        try:
+            description = driver.find_element(By.XPATH,
+                                              "//p[contains(@class, 'break-words white-space-pre-wrap mb5 text-body-small t-black--light')]")
+        except:
+            for ab in about:
+                ab.click()
+                break
+            description = driver.find_element(By.XPATH,
+                                              "//p[contains(@class, 'break-words white-space-pre-wrap mb5 text-body-small t-black--light')]")
+        description = description.text
 
-        finally:
-            break                                                    # break by getting one job profile(i.e sub-category) for each category 🔲 NOTE  ::: REMOVE THE finally BLOCK to get every JOB info of each CATEGORY
+        description_f = ""
+        for char in description:
+            if char != '\n' and char != '\r':
+                description_f = description_f + char
+            else:
+                description_f = description_f + ' '
 
+        # print(description.text)
+        other_info = driver.find_elements(By.XPATH, "//dd[contains(@class, 'text-body-small t-black--light')]")
+        no_of_employees = other_info[2].text
+        headquaters = other_info[4].text
+        states = location_list[0].split()
+        state = states[1]
 
-driver.quit()                                                        # Exit the program by closing all windows
+        cursor.execute("INSERT INTO categories(sr_no, category) VALUES(%s,%s)", (bb + 1, cat))
 
+        cursor.execute("INSERT INTO subcategories (sr_no, subcategory) VALUE (%s, %s)", (bb + 1, list_of_sub_jobs[0]))
+
+        bb += 1
+        cursor.execute("insert into states(sr_no, state) values (%s, %s)", (bb + 1, state))
+
+        cursor.execute("INSERT INTO Jobs(Company, Job_Position, Location) VALUES(%s, %s ,%s)",
+                       (company_list[mm], job_title_list[mm], location_list[mm]))
+
+        cursor.execute("INSERT INTO Company_Details(Name, Description, State, Subcategory) VALUES(%s, %s, %s, %s)",
+                       (company_list[mm], description_f, state, list_of_sub_jobs[0]))
+        conn.commit()
+
+        mm += 1
+
+    nn += 1
+
+driver.quit()
